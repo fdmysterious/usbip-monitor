7fc7bd266fcab0fedec9f367cbf9ce0804e1fe95
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2023-03-17 17:45:07 +0100

    First draft of binding, doesn't work because usbip binding triggers an udev unbind/bind event

--- __main__.py
+++ __main__.py
@@ -1,5 +1,14 @@
 from loguru import logger
 import pyudev
+import subprocess
+import traceback
+import time
+
+def usbip_bind(device_name):
+    logger.info(f"Bind device {device_name}")
+    time.sleep(1)
+    ret = subprocess.run(args=["/usr/sbin/usbip", "bind", "-b", device_name], capture_output=True, check=True)
+    logger.info(ret)
 
 if __name__ == "__main__":
     logger.info("Hello world!")
@@ -13,8 +22,14 @@ if __name__ == "__main__":
 
         if action == "bind":
             logger.info(f"Bind action for {device.sys_name}")
+            try:
+                usbip_bind(device.sys_name)
+            except Exception as exc:
+                logger.error(f"Unable to bind device: {exc}")
+                logger.debug(traceback.format_exc())
 
         elif action=="unbind":
             logger.warning(f"Unbind action for {device.sys_name}")
+            # Auto unbound from usbip
 
 
