d29a39fb6c47986fa4c8391f15dc6a492d322d93
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2023-03-17 19:05:22 +0100

    Move error catching in usbip_bind method

--- __main__.py
+++ __main__.py
@@ -7,8 +7,13 @@ import time
 def usbip_bind(device_name):
     logger.info(f"Bind device {device_name}")
     time.sleep(1)
-    ret = subprocess.run(args=["/usr/sbin/usbip", "bind", "-b", device_name], capture_output=True, check=True)
-    logger.info(ret)
+
+    try:
+       subprocess.run(args=["/usr/sbin/usbip", "bind", "-b", device_name], capture_output=True, check=True)
+    except Exception as exc:
+        logger.error(f"Unable to bind device: {exc}")
+        logger.debug(traceback.format_exc())
+
 
 if __name__ == "__main__":
     logger.info("Hello world!")
@@ -22,17 +27,9 @@ if __name__ == "__main__":
 
         if (action == "bind") and (device.driver != "usbip-host"):
             sys_name = device.sys_name
-
             logger.info(f"Bind action for {device.sys_name}")
-            try:
-                usbip_bind(sys_name)
-
-            except Exception as exc:
-                logger.error(f"Unable to bind device: {exc}")
-                logger.debug(traceback.format_exc())
+            usbip_bind(sys_name)
 
         elif action=="unbind":
             logger.warning(f"Unbind action for {device.sys_name}")
             # Auto unbound from usbip
-
-
