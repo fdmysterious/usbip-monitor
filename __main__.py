a53ac25d0b7aef3caeb44c01d2b0669b3f3cc93c
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2023-03-17 18:15:16 +0100

    Ignore devices binded by the usbip-host driver!

--- __main__.py
+++ __main__.py
@@ -20,10 +20,13 @@ if __name__ == "__main__":
     for action, device in monitor:
         logger.debug(f"{action}: {device}")
 
-        if action == "bind":
+        if (action == "bind") and (device.driver != "usbip-host"):
+            sys_name = device.sys_name
+
             logger.info(f"Bind action for {device.sys_name}")
             try:
-                usbip_bind(device.sys_name)
+                usbip_bind(sys_name)
+
             except Exception as exc:
                 logger.error(f"Unable to bind device: {exc}")
                 logger.debug(traceback.format_exc())
